38863a29d586fda6dc29863ff415f9d81b5213db
JUNGA <gop3064@gmail.com>
2020-09-24 20:59:56 +0900

    hide db url, id, pw

--- src/dbp/MyConnection.java
+++ src/dbp/MyConnection.java
@@ -1,9 +1,13 @@
 
 package dbp;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import javax.swing.JOptionPane;
  
 public class MyConnection {
@@ -21,11 +25,25 @@ public class MyConnection {
             JOptionPane.showMessageDialog(null, "Dvien is a problem is loaded and Registered");
         }
         
-        String strurl = "jdbc:sqlserver://DESKTOP-95BLMHF\\SQLEXPRESS:1433;databaseName=Automobile";
+        Properties properties = new Properties();
+
+	File file = new File("src/dbp/dbproperties.properties");
+        String dburl = null, dbid = null, dbpw = null;
+        try {
+                //Jdbc002_PropertiesTest 클래스의 위치에 properties 파일 저장
+                properties.load(new FileInputStream(file.getAbsolutePath()));
+                dburl = properties.getProperty("dburl");
+                dbid = properties.getProperty("dbid");
+                dbpw = properties.getProperty("dbpw"); 
+                
+        } catch (IOException e) {
+                System.out.println("fail to read properties:" + e);
+        }
+        
         // To make a connection
         try { 
             
-            con = DriverManager.getConnection(strurl, "pjunga", "junga");
+            con = DriverManager.getConnection(dburl, dbid, dbpw);
             JOptionPane.showMessageDialog(null, "Connected...");
             
             return con;
